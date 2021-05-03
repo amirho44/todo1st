7a0ff94fe77b0c4c085842969e682e1b5c177727
amirho44 <ah.abshari@gmail.com>
2021-05-04 00:16:01 +0430

    make App,index,header

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {}
+    render() {
+        return (<div>
+            <h1>
+                Todo Ap
+        </h1>
+            <h6>class type</h6>
+        </div>
+
+        );
+    }
+}
+
+export default App;
